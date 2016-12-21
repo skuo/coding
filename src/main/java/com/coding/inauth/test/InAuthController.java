43a76a68f1bd97cde4b97c683d387a0cc3568885
Steve Kuo <steve.kuo@makerstudios.com>
2016-12-20 21:09:20 -0800

    Add get a location

---
+++ src/main/java/com/coding/inauth/test/InAuthController.java
@@ -0,0 +1,57 @@
+package com.coding.inauth.test;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.coding.inauth.model.Location;
+import com.coding.inauth.service.data.LocationDs;
+
+@Controller
+public class InAuthController {
+    private static final Log log = LogFactory.getLog(InAuthController.class);
+
+    @Autowired
+    LocationDs locationDs;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/inauth/location/{latitude}/{longitude}/", headers = "accept=application/json")
+    @ResponseBody
+    public Location getLocation(@PathVariable float latitude, @PathVariable float longitude, HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+        log.info("getLocation: latitude=" + latitude + ", longitude=" + longitude + ", accept=" + request.getHeader("Content-Type")
+                + ", Authorization=" + request.getHeader("Authorization"));
+        Location location = null;
+        try {
+            location = locationDs.find(latitude, longitude);
+            if (location == null)
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (EmptyResultDataAccessException erdae) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        response.setHeader("Approved", "true");
+        return location;
+    }
+
+    @SuppressWarnings("unused")
+    private void printHeader(HttpServletRequest request) {
+        Enumeration<String> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String header = names.nextElement();
+            log.info("\t" + header + "=" + request.getHeader(header));
+        }
+    }
+}
--- src/main/sql/inAuth.sql
+++ src/main/sql/inAuth.sql
@@ -2,8 +2,15 @@ CREATE DATABASE if NOT EXISTS `inauth` DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `location` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `latitude` decimal(9,6),
-  `longitude` decimal(9,6),
+  `latitude` decimal(8,5),
+  `longitude` decimal(8,5),
   PRIMARY KEY (`id`),
   INDEX `lat_long` (`latitude`,`longitude`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+insert into location (latitude, longitude) values(1.1, 2.20000);
+insert into location (latitude, longitude) values(2.2, 134.07378);
+insert into location (latitude, longitude) values(34.0522, -118.2437);
+insert into location (latitude, longitude) values(37.7749, -122.4194);
+insert into location (latitude, longitude) values(32.7157, -117.1611);
+
