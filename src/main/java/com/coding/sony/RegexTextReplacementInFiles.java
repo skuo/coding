ca2b5c3bf34c7396dbe4b1b3d5f54079f148f9e1
Steve Kuo <steve.kuo@makerstudios.com>
2016-12-23 10:44:23 -0800

    translate fileAcceptFile to FileStringRegex

--- src/main/java/com/coding/sony/RegexTextReplacementInFiles.java
+++ src/main/java/com/coding/sony/RegexTextReplacementInFiles.java
@@ -6,23 +6,28 @@ public class RegexTextReplacementInFiles {
     
     public static void process(String startingDir, String regexPattern, String replacement, String fileAcceptPattern) {
         File file = new File(startingDir);
-        walkAndProcess(file);
+        walkAndProcess(file, regexPattern, replacement, fileAcceptPattern);
     }
 
-    private static void walkAndProcess(File file) {
+    private static void walkAndProcess(File file, String regexPattern, String replacement, String fileAcceptPattern) {
         if (file.isDirectory()) {
             // list the files and call walkAndProcess recursively
             File[] list = file.listFiles();
             for (File f : list)
-                walkAndProcess(f);
+                walkAndProcess(f, regexPattern, replacement, fileAcceptPattern);
         }
         else 
-            replace(file);
+            replace(file, regexPattern, replacement, fileAcceptPattern);
     }
     
-    private static void replace (File file) {
+    private static void replace (File file, String regexPattern, String replacement, String fileAcceptPattern) {
         // need to see if file matches fileAcceptPattern
-        System.out.println("replacing " + file);
+        System.out.print("checking " + file + ": ");
+        String filename = file.getName();
+        if (null == fileAcceptPattern || filename.matches(fileAcceptPattern))
+            System.out.println("replacing " + file);
+        else
+            System.out.println();
     }
     
     public static void main(String[] args) {
@@ -39,6 +44,10 @@ public class RegexTextReplacementInFiles {
         }
         if (args.length >= 4) {
             fileAcceptPattern = args[3];
+            // change fileAcceptPattern to string regex
+            fileAcceptPattern = fileAcceptPattern.replace(".","\\.");
+            fileAcceptPattern = fileAcceptPattern.replace("*", ".*");
+            fileAcceptPattern = fileAcceptPattern.replace("?", ".?");
         }
         if (startingDir != null) {
             process(startingDir, regexPattern, replacement, fileAcceptPattern);
