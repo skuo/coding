992ebf7892d2d491f57f9f773e5d9f665c839217
Steve Kuo <steve.kuo@makerstudios.com>
2017-01-17 14:15:33 -0800

    Finally able to connect to coding-config-server

--- src/main/java/com/coding/rest/HolaController.java
+++ src/main/java/com/coding/rest/HolaController.java
@@ -6,11 +6,13 @@ import java.net.UnknownHostException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HolaController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
