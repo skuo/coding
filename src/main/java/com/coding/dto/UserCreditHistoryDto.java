dd433391fb51231704f91d2675f48f84177bacac
Steve Kuo <steve.kuo@makerstudios.com>
2017-03-31 11:44:15 -0700

    Remove unused import

--- src/main/java/com/coding/dto/UserCreditHistoryDto.java
+++ src/main/java/com/coding/dto/UserCreditHistoryDto.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
